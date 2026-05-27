92bccf981883f75f57fce8e0858b7a608fb91da4
Jeremy Champion <jeremy@simkinslumber.com>
2026-05-27 17:14:53 -0600

    fix: update path

--- Backup-SQLExpressDB.bat
+++ Backup-SQLExpressDB.bat
@@ -1,8 +1,9 @@
 @echo off
 REM ------------------------------------------------------------------
-REM  Backup-SqlExpressDB.bat
-REM  Launcher for Windows Task Scheduler.
-REM  Points to JSON config and script being in the same folder as this script.
+REM Backup-SqlExpressDB.bat
+REM Launcher for Windows Task Scheduler.
+REM Points to JSON config and script being in the same folder as this script.
+REM If you need to place the .PS1 and .JSON elsewhere, just specify the full path below. 
 REM ------------------------------------------------------------------
 
 SET SCRIPT_PATH=%~dp0Backup-SqlExpressDB.ps1
